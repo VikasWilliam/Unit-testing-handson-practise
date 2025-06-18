ef363310d3130a97f4854d233d2ee4b3079313a2
VikasWilliam <vikasdenobili@outlook.com>
2025-06-18 18:01:36 +0530

    Test for basic-details and loan-application component

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,8 +1,45 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, nextFrame } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
+  it('Test label on basic details', async () => {
+    const el = await fixture(html`<basic-details></basic-details>`);
+    const lionInput = el.shadowRoot.querySelector('lion-input');
+    expect(lionInput.label).to.exist;
+  });
+
+  // it('Test type of Loan', async () => {
+  //   localStorage.setItem('type', 'Home Loan');
+  //   const el = await fixture(html`<basic-details></basic-details>`);
+  //   const lionInput = el.shadowRoot.querySelector('lion-input');
+  //   expect(lionInput.value).to.equal('Home Loan');
+  // });
+
+  it('Test Amount label on basic details', async () => {
+    const el = await fixture(html`<basic-details></basic-details>`);
+    const lionInput = el.shadowRoot.querySelector('lion-input-amount');
+    expect(lionInput.name).to.exist;
+  });
+
+  // it('Test type of Loan', async () => {
+  //   const el = await fixture(html`<basic-details></basic-details>`);
+  //   const lionInputrange = el.shadowRoot.querySelector('lion-input-range');
+  //   expect('Loan Period').to.equal('Loan Period');
+  // });
+  it('should display amount in words', async () => {
+    const el = await fixture(html`<basic-details></basic-details>`);
+
+    const amountInput = el.shadowRoot.querySelector('.amount');
+    const wordDiv = el.shadowRoot.querySelector('#word');
+
+    amountInput.value = '20000';
+    amountInput.dispatchEvent(new Event('keyup'));
+
+    await el.updateComplete;
+
+    expect(wordDiv.innerHTML.trim().length).to.be.greaterThan(0);
+  });
 });
--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -1,7 +1,35 @@
-import { html, fixture, expect } from '@open-wc/testing';
-
+import { html, fixture, expect, aTimeout } from '@open-wc/testing';
 import '../loan-application.js';
 
 describe('LoanApplication', () => {
-  // Write test cases inside this block
+  let el;
+
+  beforeEach(async () => {
+    el = await fixture(html`<loan-application></loan-application>`);
+  });
+
+  it('should have default title and counter', () => {
+    expect(el.title).to.equal('Hey there');
+    expect(el.counter).to.equal(5);
+  });
+
+  it('should render dashboard component', () => {
+    const dashboard = el.shadowRoot.querySelector('dash-board');
+    expect(dashboard).to.exist;
+  });
+
+  it('should increment the counter when __increment is called', () => {
+    el.__increment();
+    expect(el.counter).to.equal(6);
+  });
+
+  it('should reflect updated counter on multiple __increment calls', () => {
+    el.__increment();
+    el.__increment();
+    expect(el.counter).to.equal(7);
+  });
+
+  it('is accessible', async () => {
+    await expect(el).to.be.accessible();
+  });
 });
